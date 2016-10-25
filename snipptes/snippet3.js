371327bc77c5ff6114613c5db8e6db41a935ae5f
lbrendanl <lee33@uw.edu>
2016-10-25 11:44:19 -0700

    Added link to tv api

--- snipptes/snippet3.js
+++ snipptes/snippet3.js
@@ -2,3 +2,5 @@ var cols = [
     { id: "popularity", dataType: tableau.dataTypeEnum.float },
     { id: "name", dataType: tableau.dataTypeEnum.string }
 ];
+
+// https://developers.themoviedb.org/3/tv/get-top-rated-tv
