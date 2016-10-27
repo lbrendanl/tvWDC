a64dd9f38eff48fd97da40decc21fb5585492a1d
lbrendanl <lee33@uw.edu>
2016-10-27 15:01:41 -0700

    Snippet 7 cleanup

--- snipptes/snippet7.js
+++ snipptes/snippet7.js
@@ -1,2 +1,3 @@
-var api_key = "8922bff85ef645a09730d7c1836c3edf",
-"http://api.themoviedb.org/3/tv/popular?api_key=" + api_key;
+var api_key = "8922bff85ef645a09730d7c1836c3edf";
+
+var connectionUrl = "http://api.themoviedb.org/3/tv/popular?api_key=" + api_key;
