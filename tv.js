0da2b6b1cf885781e357fdbd5f3e9eec2359c62d
Brendan Lee <lee33@uw.edu>
2019-01-11 13:49:35 -1000

    Updated to https

--- tv.js
+++ tv.js
@@ -4,8 +4,8 @@
     var num_pages = 10;
 
     var api_key = "8922bff85ef645a09730d7c1836c3edf",
-        base_uri = "http://api.themoviedb.org/3/",
-        images_uri =  "http://image.tmdb.org/t/p/w500";
+        base_uri = "https://api.themoviedb.org/3/",
+        images_uri =  "https://image.tmdb.org/t/p/w500";
 
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
